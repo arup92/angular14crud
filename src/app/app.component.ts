790b57710cec4bed16c41166aeffbbecfdc962f1
arup92 <arup.debnath3@gmail.com>
2022-07-10 01:07:09 +0530

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import {MatDialog} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular14crud';
+
+  constructor(private dialog : MatDialog) {
+
+  }
+
+  // Open the dialog component  on click
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '30%'
+    });
+  }
 }
